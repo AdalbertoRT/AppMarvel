ebc7079bd86719211c30953245be582192910922
Adalberto R. Teixeira <adalbertoart@gmail.com>
2021-10-24 01:53:30 -0300

    comecando edicao dos herois

---
+++ src/pages/edited-heroes/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const PageEditedHeroes = styled.SafeAreaView`
+  flex: 1;
+  background-color: #151515;
+  padding: 5;
+`;
+export const EditedHeroesList = styled.FlatList`
+  flex: 1;
+`;
